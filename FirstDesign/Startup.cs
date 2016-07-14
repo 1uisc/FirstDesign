fe6dc605fdce9fcd8a1f9b6099d1bd481ddb3d88
luisc <luisc@LUISC-PC>
2016-07-14 14:32:28 -0300

    Add project files.

---
+++ FirstDesign/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstDesign.Startup))]
+namespace FirstDesign
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
